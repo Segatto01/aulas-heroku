251fee4e6f7a9bc420d881de660457e629861714
Segatto01 <88349740+Segatto01@users.noreply.github.com>
2022-01-26 15:46:46 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,15 +4,34 @@
 
 ?> 
 
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula_Heroku</title>
-</head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title> Segatto Doações </title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+<link rel="stylesheet" href="index.css">
+</head> 
 <body>
-    <h1>Aula Heroku</h1>
-</body>
-</html>
+    <div id="Login-container">
+    <h1>Entre para Adotar</h1>
+    <form action="">
+        <label for= "email">E-mail</label>
+    <input type= "email" name="email" id="email" placeholder="Digite seu email" autocomplete="off">
+    <label for="password">Senha</label>
+    <input type="password" name="password" id="password" placeholder="Digite a sua senha">
+    <a href="#" id="forgot-pass">Esqueceu a senha?</a>
+    <input type="submit" value="Login">
+    </form>
+    <div id="social-container">          
+            </div>
+            <div id="register-container">
+                <p>Ainda nao tem uma conta?</p>
+                <a hrerf="#">Cadastre-se</a>
+            </div>
+
+        </div>
+
+            </body>
+            </html>
